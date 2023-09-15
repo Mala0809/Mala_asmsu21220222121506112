cb4114f4e278928831328c505106f7f96017b8af
Mala0809 <144647783+Mala0809@users.noreply.github.com>
2023-09-15 11:54:38 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,24 @@
-def fact(n):
-  if(n==0):
-    return (1)
-  else:
-    return (n*fact(n-1))
 
 
-m=int(input("Enter n:"))
-R=fact(m)
-print(R)
+# Define the base class player
+class Player:
+  def play(self):
+    print("The player is playing cricket.")
+
+# Define the derived class Batsman
+class Batsman(Player):
+  def play(self):
+    print("The batsman is batting.")
+
+# Define the derived class Bowler
+class Bowler(Player):
+  def play(self):
+    print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
